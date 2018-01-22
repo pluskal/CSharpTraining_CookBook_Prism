4eb005079167e2a32771c0c534c8a9c0912f4aff
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-01-22 21:57:59 +0100

    Add basic app structure.
    ShellWindow, Bootstrapper, IoCInstaller.

---
+++ CookBook.App/Bootstrapper.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using Castle.Windsor.Installer;
+using CookBook.App.Views;
+using Prism.Logging;
+using Prism.Modularity;
+using Prism.Mvvm;
+using PrismContrib.WindsorExtensions;
+
+namespace CookBook.App
+{
+    public class Bootstrapper : WindsorBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            this.Container.Install(FromAssembly.InThisApplication());
+        }
+
+        protected override ILoggerFacade CreateLogger()
+        {
+            return new TextLogger();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            var moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            // TODO moduleCatalog.AddModule(typeof(<moduleName>));
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.SetDefaultViewModelFactory(type => this.Container.Resolve(type));
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<ShellWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ CookBook.App/IocInstaller.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Reflection;
+using Castle.Facilities.Logging;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
+using Castle.Services.Logging.Log4netIntegration;
+using Castle.Windsor;
+using CookBook.App.Infrastructure.Interfaces;
+using CookBook.App.Views;
+using Prism.Modularity;
+using IConfigurationStore = Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore;
+
+namespace CookBook.App
+{
+    public class IocInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<ShellWindow>());
+            container.AddFacility<LoggingFacility>(f => f.LogUsing<Log4netFactory>());
+            container.AddFacility<TypedFactoryFacility>();
+
+            var currentDirectoryAssemblyFilter = new AssemblyFilter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "CookBook.*");
+
+            container.Register(
+                Classes.FromAssemblyInDirectory(currentDirectoryAssemblyFilter)
+                    .BasedOn<IModule>()
+                    .LifestyleSingleton());
+
+            container.Register(
+                Classes.FromAssemblyInDirectory(currentDirectoryAssemblyFilter)
+                    .BasedOn<IControllerBase>()
+                    .LifestyleSingleton());
+
+            container.Register(
+                Classes.FromAssemblyInDirectory(currentDirectoryAssemblyFilter)
+                    .Where(p => p.Name.EndsWith("ViewModel"))
+                    .LifestyleTransient());
+
+            container.Register(
+                Classes.FromAssemblyInDirectory(currentDirectoryAssemblyFilter)
+                    .Where(p => p.Name.EndsWith("View"))
+                    .LifestyleTransient());
+        }
+        
+    }
+}
